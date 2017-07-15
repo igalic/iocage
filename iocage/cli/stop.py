9eb102ecfb4654a3f82561642c97c02103053004
Stefan Grönke <stefan@gronke.net>
2017-07-15 17:48:15 +0200

    cli commands 'start', 'stop' and 'list'

--- iocage/cli/stop.py
+++ iocage/cli/stop.py
@@ -24,8 +24,7 @@
 """stop module for the cli."""
 import click
 
-import iocage.lib.ioc_common as ioc_common
-import iocage.lib.iocage as ioc
+from iocage.lib.Jail import Jail
 
 __rootcmd__ = True
 
@@ -40,16 +39,5 @@ def cli(rc, jails):
     Looks for the jail supplied and passes the uuid, path and configuration
     location to stop_jail.
     """
-    if not jails and not rc:
-        ioc_common.logit({
-            "level"  : "ERROR",
-            "message": 'Usage: iocage stop [OPTIONS] JAILS...\n'
-                       '\nError: Missing argument "jails".'
-        })
-        exit(1)
-
-    if rc:
-        ioc.IOCage(rc=rc, silent=True).stop()
-    else:
-        for jail in jails:
-            ioc.IOCage(jail, rc=rc).stop()
+    for jail in jails:
+        Jail(jail).stop()
