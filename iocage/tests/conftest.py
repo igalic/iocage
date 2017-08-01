8e62779d2643ecc16f5e8eedb27b83ab2f3b1ff6
Igor Galić <i.galic@brainsware.org>
2017-08-01 09:13:04 +0200

    chore: lint clean
     Conflicts:
    	iocage/tests/conftest.py

--- iocage/tests/conftest.py
+++ iocage/tests/conftest.py
@@ -13,21 +13,30 @@ if iocage_lib_dir not in sys.path:
     sys.path = [iocage_lib_dir] + sys.path
 
 _force_clean = False
+
+
 def pytest_addoption(parser):
     parser.addoption("--force-clean", action="store_true",
         help="Force cleaning the /iocage-test dataset")
 
+
 def pytest_generate_tests(metafunc):
     _force_clean = metafunc.config.getoption("force_clean")
+
+
+print("force_clean?", _force_clean)
+
 
 @pytest.fixture
 def force_clean():
     return _force_clean
 
+
 @pytest.fixture
 def zfs():
     import libzfs
     return libzfs.ZFS(history=True, history_prefix="<iocage>")
+
 
 @pytest.fixture
 def pool(zfs, logger):
@@ -50,10 +59,12 @@ def pool(zfs, logger):
 
     return active_pool
 
+
 @pytest.fixture
 def logger():
     import Logger
     return Logger.Logger()
+
 
 @pytest.fixture
 def root_dataset(force_clean, zfs, pool):
@@ -63,7 +74,7 @@ def root_dataset(force_clean, zfs, pool):
     if force_clean:
         try:
             dataset = zfs.get_dataset(dataset_name)
-            helper_functionsunmount_and_destroy_dataset_recursive(dataset)
+            helper_functions.unmount_and_destroy_dataset_recursive(dataset)
         except:
             pass
 
@@ -81,7 +92,7 @@ def root_dataset(force_clean, zfs, pool):
     yield dataset
 
     if force_clean:
-        helper_functionsunmount_and_destroy_dataset_recursive(dataset)
+        helper_functions.unmount_and_destroy_dataset_recursive(dataset)
 
 @pytest.fixture
 def host(root_dataset, logger, zfs):
@@ -89,7 +100,8 @@ def host(root_dataset, logger, zfs):
     host = Host.Host(root_dataset=root_dataset, logger=logger, zfs=zfs)
     yield host
     del host
-    
+
+
 @pytest.fixture
 def release(host, logger, zfs):
     import Release
