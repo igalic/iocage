29848daa7956aff1051c1f2ae497742943b87016
Igor Galić <i.galic@brainsware.org>
2017-08-03 23:32:56 +0200

    httperror is a subclass of OSerror

--- iocage/tests/test_Release.py
+++ iocage/tests/test_Release.py
@@ -6,8 +6,18 @@ import hypothesis.strategies as st
 
 class TestRelease(object):
 
-    @given(name=st.text())
-    def test_fetch_release(self, name, host, logger, zfs, root_dataset):
+    def valid_release_name():
+        from string import ascii_letters
+        from string import digits
 
-        release = Release.Release(name=name, host=host, logger=logger, zfs=zfs)
-        release.fetch()
+        alphabet = ascii_letters + digits + '_-.'
+        return st.text(alphabet=alphabet, min_size=1, max_size=255)
+
+    @given(name=valid_release_name())
+    def test_fetch_release(self, name, host, logger, zfs, root_dataset):
+        try:
+            release = Release.Release(name=name, host=host, logger=logger, zfs=zfs)
+            release.fetch()
+        except OSError as err:
+            if err.code is 404:
+                pass
